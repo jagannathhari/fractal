ead79e0fc43f3e0a7d048a7ad6d5484901efca35
Jagannath Hari <jagannathhari17@gmail.com>
2024-09-23 14:51:24 +0530

    Added suport for compiling for windows in gitbash

--- build.sh
+++ build.sh
@@ -21,6 +21,17 @@ if [[ "$OS" == "Linux" ]]; then
 elif [[ "$OS" == "Darwin" ]]; then
     RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION}/raylib-${RAYLIB_VERSION}_macos.tar.gz"
     RAYLIB_FOLDER="raylib-${RAYLIB_VERSION}_macos"
+elif [[ "$OS" == *"NT"* ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION}/raylib-${RAYLIB_VERSION}_windows_amd64.zip"
+        RAYLIB_FOLDER="raylib-${RAYLIB_VERSION}_windows_amd64"
+    elif [[ "$ARCH" == "i386" ]]; then
+        RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION}/raylib-${RAYLIB_VERSION}_windows_i386.zip"
+        RAYLIB_FOLDER="raylib-${RAYLIB_VERSION}_windows_i386"
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
 else
     echo "Unsupported OS: $OS"
     exit 1
@@ -28,7 +39,17 @@ fi
 
 if [ ! -d "$RAYLIB_FOLDER" ]; then
     echo "Downloading raylib..."
-    wget $RAYLIB_URL -O raylib_archive
+    if command -v wget > /dev/null; then
+        wget $RAYLIB_URL -O raylib_archive
+    else
+        curl -L $RAYLIB_URL -o raylib_archive
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to download raylib."
+        exit 1
+    fi
+
     if [[ $RAYLIB_URL == *.tar.gz ]]; then
         tar -xzf raylib_archive
     elif [[ $RAYLIB_URL == *.zip ]]; then
@@ -43,4 +64,10 @@ LIB_PATH="./$RAYLIB_FOLDER/lib/"
 
 mkdir -p ./bin
 
-cc "./src/fractal.c" "./src/world.c" -o ./bin/fractal -I$INCLUDE_PATH_RAYLIB -I$INCLUDE_PATH -L$LIB_PATH -l:libraylib.a -lm
+SOURCES="./src/fractal.c ./src/world.c"
+
+if [[ "$OS" == "Linux" || "$OS" == "Darwin" ]]; then
+    cc $SOURCES -o ./bin/fractal -I$INCLUDE_PATH_RAYLIB -I$INCLUDE_PATH -L$LIB_PATH -l:libraylib.a -lm
+elif [[ "$OS" == *"NT"* ]]; then
+    gcc $SOURCES -o ./bin/fractal.exe -I$INCLUDE_PATH_RAYLIB -I$INCLUDE_PATH -L$LIB_PATH -l:ribraylib.a -lm
+fi

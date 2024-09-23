658e3599d711f504924440a9b37c8e15a02445b3
Jagannath Hari <jagannathhari17@gmail.com>
2024-09-23 13:27:26 +0530

    Changed Build script to automatically download dependency

--- build.sh
+++ build.sh
@@ -2,12 +2,45 @@
 
 set -xe
 
-INCLUDE_PATH_RAYLIB="./raylib-5.0_linux_amd64/include/"
-INCLUDE_PATH="./src/include/"
-LIB_PATH="./raylib-5.0_linux_amd64/lib/"
+ARCH=$(uname -m)
+OS=$(uname)
 
+RAYLIB_VERSION="5.0"
+
+if [[ "$OS" == "Linux" ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION}/raylib-${RAYLIB_VERSION}_linux_amd64.tar.gz"
+        RAYLIB_FOLDER="raylib-${RAYLIB_VERSION}_linux_amd64"
+    elif [[ "$ARCH" == "i386" || "$ARCH" == "i686" ]]; then
+        RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION}/raylib-${RAYLIB_VERSION}_linux_i386.tar.gz"
+        RAYLIB_FOLDER="raylib-${RAYLIB_VERSION}_linux_i386"
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+elif [[ "$OS" == "Darwin" ]]; then
+    RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION}/raylib-${RAYLIB_VERSION}_macos.tar.gz"
+    RAYLIB_FOLDER="raylib-${RAYLIB_VERSION}_macos"
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+if [ ! -d "$RAYLIB_FOLDER" ]; then
+    echo "Downloading raylib..."
+    wget $RAYLIB_URL -O raylib_archive
+    if [[ $RAYLIB_URL == *.tar.gz ]]; then
+        tar -xzf raylib_archive
+    elif [[ $RAYLIB_URL == *.zip ]]; then
+        unzip raylib_archive
+    fi
+    rm raylib_archive
+fi
+
+INCLUDE_PATH_RAYLIB="./$RAYLIB_FOLDER/include/"
+INCLUDE_PATH="./src/include/"
+LIB_PATH="./$RAYLIB_FOLDER/lib/"
 
 mkdir -p ./bin
 
-
-gcc "./src/fractal.c" "./src/world.c" -o ./bin/fractal -I$INCLUDE_PATH_RAYLIB -I$INCLUDE_PATH -L$LIB_PATH -l:libraylib.a -lm
+cc "./src/fractal.c" "./src/world.c" -o ./bin/fractal -I$INCLUDE_PATH_RAYLIB -I$INCLUDE_PATH -L$LIB_PATH -l:libraylib.a -lm

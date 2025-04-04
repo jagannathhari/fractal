cde087c161dd06e6fffa5819021e8b3d095c9aa8
Jagannath Hari <jagannathhari17@gmail.com>
2025-04-04 15:23:39 +0530

    Refactor the code.

--- src/fractal.c
+++ src/fractal.c
@@ -20,7 +20,8 @@
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
-typedef struct {
+typedef struct
+{
     float x;
     float y;
     float angle;
@@ -30,22 +31,28 @@ float length = 10;
 float angle = 0;
 char **productions = NULL;
 
-void process_char(char p, State *s) {
+void process_char(char p, State *s)
+{
     if(p>='a' && p<='z'){
         s->x += length * cos(s->angle);
         s->y += length * sin(s->angle);
         return;
     }
-    if(p>='A' && p<='Z'){
+    if(p>='A' && p<='Z')
+    {
         DrawLineEx(
             (Vector2){s->x, s->y},
-            (Vector2){s->x + length * cos(s->angle), s->y + length * sin(s->angle)}, 3,
+            (Vector2){s->x + length * cos(s->angle),
+                        s->y + length * sin(s->angle)},
+            3,
             RED);
+
         s->x += length * cos(s->angle);
         s->y += length * sin(s->angle);
         return;
     }
-    switch (p) {
+    switch (p)
+    {
     case '-':
         s->angle -= angle;
         break;
@@ -55,28 +62,38 @@ void process_char(char p, State *s) {
     }
 }
 
-void process_string(char *p, State *s) {
-    while (*p) {
+void process_string(char *p, State *s)
+{
+    while (*p)
+    {
         process_char(*p++, s);
     }
 }
 
-char *expand_production(char *axiom, unsigned int level){
+char *expand_production(char *axiom, unsigned int level)
+{
     char *previous = Vector(*previous);
     char *current = NULL;
-    while(*axiom){
+    while(*axiom)
+    {
         vector_append(previous, *axiom++);
     }
-    while(level--){
+    while(level--)
+    {
         current = Vector(*current);
         size_t len = vector_length(previous);
-        for(size_t i = 0; i< len;i++){
-            if(productions[previous[i]]){
+        for(size_t i = 0; i< len;i++)
+        {
+            if(productions[previous[i]])
+            {
                 char *temp = productions[previous[i]];
-                while(*temp){
+                while(*temp)
+                {
                     vector_append(current, *temp++);
                 }
-            }else{
+            }
+            else
+            {
                 vector_append(current,previous[i]);
             }
         }
@@ -87,7 +104,8 @@ char *expand_production(char *axiom, unsigned int level){
     return previous;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     if(argc != 2){
 
         printf("%f",GetFrameTime());
@@ -97,7 +115,8 @@ int main(int argc, char **argv) {
 
     World world;
 
-    if(!load_world(argv[1], &world)){
+    if(!load_world(argv[1], &world))
+    {
         printf("Unable to load world. %s is invalid or currpted",argv[1]);
         return 0;
     }
@@ -121,38 +140,48 @@ int main(int argc, char **argv) {
 
     char *result = expand_production(world.axioms,world.level);
 
-    while (!WindowShouldClose()) {
-        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+    while (!WindowShouldClose())
+    {
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+        {
             Vector2 mousePosition = GetMousePosition();
-            if (!isPanning) {
+            if (!isPanning)
+            {
                 previousMousePosition = mousePosition; 
                 isPanning = true;
             }
 
-            Vector2 mouseDelta = (Vector2){mousePosition.x - previousMousePosition.x, mousePosition.y - previousMousePosition.y};
+            Vector2 mouseDelta = (Vector2){mousePosition.x - previousMousePosition.x,
+                                           mousePosition.y - previousMousePosition.y
+                                          };
             camera.target.x -= mouseDelta.x / camera.zoom; 
             camera.target.y -= mouseDelta.y / camera.zoom;
             previousMousePosition = mousePosition; 
 
-        } else {
+        }
+        else
+        {
             isPanning = false;
         }
 
         camera.zoom += ((float)GetMouseWheelMove() * 0.05f);
 
-        if (IsKeyPressed(KEY_R)) {
+        if (IsKeyPressed(KEY_R))
+        {
             camera.zoom = 1.0f;
             camera.rotation = 0.0f;
             camera.target = (Vector2){world.x + 20.0f, world.y + 20.0f}; // Reset to initial position
         }
 
-        if(IsKeyReleased(KEY_J)){
+        if(IsKeyReleased(KEY_J))
+        {
             world.level = max(MIN_LEVEL,world.level-1);
             free_vector(result);
             result = expand_production(world.axioms,world.level);
         }
 
-        if(IsKeyReleased(KEY_K)){
+        if(IsKeyReleased(KEY_K))
+        {
             world.level = min(MAX_LEVEL,world.level+1);
             free_vector(result);
             result = expand_production(world.axioms,world.level);
@@ -174,8 +203,10 @@ int main(int argc, char **argv) {
     CloseWindow();
 
     free(world.axioms);
-    for(int i = 0; i< 256 ; i++){
-        if(world.productions[i]){
+    for(int i = 0; i< 256 ; i++)
+    {
+        if(world.productions[i])
+        {
             free(world.productions[i]);
         }
     }
